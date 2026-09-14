1bf06a0aa07909266e948b389f6e875a1c842b88
Prashant <@prashantyadav6400@gmail.com>
2026-09-14 18:50:20 +0530

    modify

--- Firstcode.py/string.py
+++ Firstcode.py/string.py
@@ -1,4 +1,19 @@
+# 1 
 first_name = "Tony"
 last_name ="Stark"
 age = "53"
 height = "1.85m"
+
+#2
+first_name = "Tony"
+last_name = "Stark"
+age = "53"
+height = "1.85m"
+
+print("Tony is secretly a superhero " +
+      first_name + " " +
+      last_name + " " +
+      "age: " + age + " " +
+      "height: " + height)
+
+print(f"Tony is secretly a superhero {first_name} {last_name} age: {age} height: {height}")
